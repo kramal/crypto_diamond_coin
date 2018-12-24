db7ae45298b073dc8b495f688bc1385b772825a5
Almas Mirzakhmetov <kramal@gmail.com>
2018-12-24 11:05:39 +0600

    dynamic difficulty

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-const INITIAL_DIFFICULTY = 3;
+const MINE_RATE = 1000;
+
+const INITIAL_DIFFICULTY = 1;
 
 const GENESIS_DATA = {
   timestamp: 1,
@@ -9,4 +11,4 @@ const GENESIS_DATA = {
   data: []
 };
 
-module.exports = { GENESIS_DATA };
+module.exports = { GENESIS_DATA, MINE_RATE };
